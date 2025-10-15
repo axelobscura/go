0013f1365e61855cc39e3f43aaef4c7a595a8d06
Axel Laurent <axosar@gmail.com>
2025-10-14 20:36:09 -0600

    types

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	expectedReturnRate := 5.5
 	var investmentDuration float64 = 10
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100*float64(investmentDuration)), investmentDuration)
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), investmentDuration)
+
+	fmt.Println("Future Real Value of Investment:", futureRealValue)
 	fmt.Println("Future Value of Investment:", futureValue)
 }
